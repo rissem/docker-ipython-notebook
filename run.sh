9ffa5b08fe499ce0ff9e6b1d6b5f497a045d4edb
Mike Risse <rissem@gmail.com>
2014-06-08 14:30:12 -0400

    allow projects w/o notebooks in git root

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ else
     cd /tmp/notebook && git clone https://github.com/$GIT_REPO
 fi
 
-ipython notebook --no-browser --ip=0.0.0.0 --port=8080 --notebook-dir=/tmp/notebook/${GIT_REPO/*\/}
+ipython notebook --no-browser --ip=0.0.0.0 --port=8080 --notebook-dir=/tmp/notebook/${GIT_REPO/*\/}/${REPO_DIR}
